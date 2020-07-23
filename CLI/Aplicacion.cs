112a0c7afbd71cebc35dc9f6e7aaa4ecc7facf89
walter sanchez <walter.fceia@outlook.com>
2020-07-23 15:42:54 -0300

    Progarma iniciado

---
+++ CLI/Aplicacion.cs
@@ -0,0 +1,23 @@
+﻿using Servicios;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace CLI
+{
+    public class Aplicacion
+    {
+        public string Archivo { get; set; }
+        /// <summary>
+        /// Ejecuta la aplicación principal
+        /// </summary>
+        public void Run()
+        {
+            ServiciosImportacion imp = new ServiciosImportacion();
+            imp.ImportarCSV(Archivo);
+            //Console.WriteLine(Archivo);
+            //Console.ReadLine();
+        }
+    }
+}
---
+++ CLI/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // DECLARACION
+            Aplicacion app;
+
+            //ASIGNACION
+            app = new Aplicacion();
+
+            app.Archivo = "C:\\cursocsharp2020\\libros.csv";
+            app.Run();
+
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
